0524e4edc179037bb08bc6d4d5c0a6caeeda4382
Bishnu podo roy Kaushik <bprk52524@gmail.com>
2025-11-24 00:40:02 +0600

    Revert "23 night 800 2"

---
+++ A_Sakurako_and_Kosuke.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+void solve(){
+    int n;cin>>n;
+    if(n%2==0){
+        cout<<"Sakurako"<<endl;
+    }else{
+        cout<<"Kosuke"<<endl;
+    }
+}
+int main(){
+    int t;cin>>t;
+    while(t--){
+        solve();
+    }
+}
